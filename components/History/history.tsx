3823267036870d22e60a30658d5fd4b11861d568
matheusgongo <matheusgongo@ucl.br>
2024-06-20 21:15:05 -0300

    rename history component

--- components/History/history.tsx
+++ components/History/history.tsx
@@ -55,7 +55,6 @@ const HistoryPage: React.FC = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-8">Histórico Financeiro</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <PortfolioEvolutionChart data={portfolioEvolution} />
         <Timeline events={timelineEvents} />
